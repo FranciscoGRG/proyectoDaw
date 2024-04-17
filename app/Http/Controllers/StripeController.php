625ed2525bdf442716cb3a414cce56ebd0cec9a6
FranciscoGRG <fgrcalifa@gmail.com>
2024-04-17 12:24:10 +0200

    añadido el cobro de comisiones por venta

--- app/Http/Controllers/StripeController.php
+++ app/Http/Controllers/StripeController.php
@@ -6,11 +6,6 @@ use Illuminate\Http\Request;
 
 class StripeController extends Controller
 {
-    // public function index()
-    // {
-    //     return view('index');
-    // }
-
     //Esta funcion recibe el nombre del producto, cantidad y precio y redirecciona a la página de Stripe para realizar el pago
     public function checkout(Request $request)
     {
@@ -42,3 +37,47 @@ class StripeController extends Controller
         return view('index');
     }
 }
+
+
+
+
+//Esta es la funcion actualizada para cobrar una comision del 3% al vendedor. Este necesita una cuenta de Stripe para poder recibir el dinero de la venta
+    // public function checkout(Request $request)
+    // {
+    //     Stripe::setApiKey(config('stripe.sk'));
+
+    //     // Calcula el monto de la comisión (3% del precio total)
+    //     $comision = $request->precio * $request->cantidad * 0.03;
+
+    //     // Calcula el monto a transferir al vendedor (precio total - comisión)
+    //     $monto_vendedor = ($request->precio * $request->cantidad) - $comision;
+
+    //     // Crea la sesión de pago en Stripe
+    //     $session = Session::create([
+    //         'payment_method_types' => ['card'],
+    //         'line_items' => [
+    //             [
+    //                 'price_data' => [
+    //                     'currency' => 'eur',
+    //                     'product_data' => [
+    //                         'name' => $request->producto,
+    //                     ],
+    //                     'unit_amount' => $request->precio,
+    //                 ],
+    //                 'quantity' => $request->cantidad,
+    //             ],
+    //         ],
+    //         'payment_intent_data' => [
+    //             'application_fee_amount' => (int) $comision, // Comisión
+    //             'transfer_data' => [
+    //                 'destination' => '{{ VENDEDOR_ACCOUNT_ID }}', // ID de la cuenta del vendedor
+    //                 'amount' => (int) $monto_vendedor, // Monto para el vendedor
+    //             ],
+    //         ],
+    //         'mode' => 'payment',
+    //         'success_url' => route('success'),
+    //         'cancel_url' => route('index'),
+    //     ]);
+
+    //     return redirect()->away($session->url);
+    // }
